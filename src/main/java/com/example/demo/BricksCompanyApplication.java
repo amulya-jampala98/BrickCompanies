f74de5e0a2f2a39dba769093dca1a3fb659fae57
amulya-jampala98 <amulya.jampala.98@gmail.com>
2025-01-27 15:43:50 -0800

    Initial Commit, DynamoDB configuration

--- src/main/java/com/example/demo/BricksCompanyApplication.java
+++ src/main/java/com/example/demo/BricksCompanyApplication.java
@@ -1,13 +1,28 @@
 package com.example.demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.ComponentScan;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BricksCompanyApplication {
+@ComponentScan(basePackages = {"com.example.demo"})
+
+
+public class BricksCompanyApplication implements CommandLineRunner {
+
+	@Autowired
+	private DynamoDBService dynamoDBService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BricksCompanyApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		// Invoke the DynamoDB service
+		dynamoDBService.printAllItems("BrickCompanies");
+	}
 }
